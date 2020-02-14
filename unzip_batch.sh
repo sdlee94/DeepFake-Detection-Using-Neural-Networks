ec5ff8acf0f7fa4f3f163179e8bf1d598667c6f7
Stephen Lee <stephen_lee_94@hotmail.com>
2020-02-13 23:44:21 -0800

    fix unzip_batch.sh

--- unzip_batch.sh
+++ unzip_batch.sh
@@ -4,5 +4,5 @@
 # name its associated metadata file with 'metatdata_i-j.csv', and remove the zip file
 batch_id=$(echo $1 | awk '{ gsub("deepfake-detection-faces-part-", ""); print }' | awk '{ gsub(".zip", ""); print }')
 
-unzip data/$1;
-mv data/metadata.csv > data/metadata$batch_id.csv
+unzip data/deepfake-detection-faces-part-$1.zip -d data;
+mv data/metadata.csv data/metadata_$batch_id.csv
