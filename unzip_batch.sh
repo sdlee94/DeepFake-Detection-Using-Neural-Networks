21f705dd366d1961e1bf966c38b37dd76b163f67
Stephen Lee <stephen_lee_94@hotmail.com>
2020-02-21 17:18:10 -0800

    add code to append batch metadata to data/metadata.csv in unzip_batch.sh

--- unzip_batch.sh
+++ unzip_batch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # unzip a deepfake-detection-faces-part-i-j.zip file,
-# name its associated metadata file with 'metatdata_i-j.csv', and remove the zip file
+# name its associated metadata file with 'metatdata_i-j.csv', and append it to data/metadata.csv
 unzip data/deepfake-detection-faces-part-$1.zip -d data;
 mv data/metadata.csv data/metadata_$1.csv;
+awk 'NR>1' data/metadata_$1.csv >> data/metadata.csv;
+
 #rm data/deepfake-detection-faces-part-$1.zip
