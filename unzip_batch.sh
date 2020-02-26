b56eee8ce865010355ff5944208516943649f7d0
Stephen Lee <stephen_lee_94@hotmail.com>
2020-02-25 16:28:41 -0800

    add code to obtain single metadata file as comment in unzip_batch.sh

--- unzip_batch.sh
+++ unzip_batch.sh
@@ -4,6 +4,9 @@
 # name its associated metadata file with 'metatdata_i-j.csv', and append it to data/metadata.csv
 unzip data/deepfake-detection-faces-part-$1.zip -d data;
 mv data/metadata.csv data/metadata_$1.csv;
-awk 'NR>1' data/metadata_$1.csv >> data/metadata.csv;
+
+# Run the following after unzipping everything to obtain a single metadata file:
+# awk 'NR>1' data/metadata*.csv > data/metadata.csv;
+# sed -i '1 i\filename,split,original,label
 
 #rm data/deepfake-detection-faces-part-$1.zip
